7e33e9480085caacfa90268b4e016363ea7066bf
raj12359 <rajeevkumarsingh913572@gmail.com>
2025-12-12 23:38:33 +0530

    add url frontend

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // -------------------- CORS --------------------
 app.use(cors({
-  origin: "http://127.0.0.1:5500", // or your local frontend URL
+  origin: "https://spro-custmercare.netlify.app/", // or your local frontend URL
   credentials: true
 }));
 
